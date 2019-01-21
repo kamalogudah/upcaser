24d7c9f0a3402d6dac1a0be85f0be6dda77fece8
kamalogudah <mcpaul2058@gmail.com>
2019-01-21 19:00:30 +0300

    Initial Commit

---
+++ lib/upcaser.ex
@@ -0,0 +1,18 @@
+defmodule Upcaser do
+  @moduledoc """
+  Documentation for Upcaser.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Upcaser.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/upcaser_test.exs
@@ -0,0 +1,8 @@
+defmodule UpcaserTest do
+  use ExUnit.Case
+  doctest Upcaser
+
+  test "greets the world" do
+    assert Upcaser.hello() == :world
+  end
+end
